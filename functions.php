1dda68b4f4e74a066cab90d602e4b9eb49c3361e
markon87 <marfy87@gmail.com>
2016-04-02 18:31:38 +0200

    setting kibango theme

--- functions.php
+++ functions.php
@@ -1,18 +1,18 @@
 <?php 
 	
-function basicmultipager_script_enqueue() {
+function kibango_script_enqueue() {
 	wp_enqueue_style('bootstrapcss', get_template_directory_uri().'/css/bootstrap.min.css', array(), '1.0.0', 'all');
-	wp_enqueue_style('customstyle', get_template_directory_uri().'/css/basicmultipager.css', array(), '1.0.0', 'all');
+	wp_enqueue_style('customstyle', get_template_directory_uri().'/css/kibango.css', array(), '1.0.0', 'all');
 
 	wp_enqueue_script('bootstrapjs', get_template_directory_uri().'/js/bootstrap.min.js', array(), '1.0.0', true);
 	wp_enqueue_script('customjs', get_template_directory_uri().'/js/main.js', array(), '1.0.0', true);
 }
 
-add_action('wp_enqueue_scripts', 'basicmultipager_script_enqueue');
+add_action('wp_enqueue_scripts', 'kibango_script_enqueue');
 
-function basicmultipager_theme_setup(){
+function kibango_theme_setup(){
 	add_theme_support('menus');
 
 	register_nav_menu('primary', 'Primary Header Navigation');
 }
-add_action('init', 'basicmultipager_theme_setup');
+add_action('init', 'kibango_theme_setup');
--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Basic multipager</title>
+		<title>Kibango kennel</title>
 		<?php wp_head(); ?>
 	</head>
 
