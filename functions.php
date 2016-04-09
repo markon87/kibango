a3f1ff3521545d7480ab49165a496dcd9668eba8
markon87 <marfy87@gmail.com>
2016-04-09 13:52:05 +0200

    plug ins

--- functions.php
+++ functions.php
@@ -2,9 +2,13 @@
 	
 function kibango_script_enqueue() {
 	wp_enqueue_style('bootstrapcss', get_template_directory_uri().'/css/bootstrap.min.css', array(), '1.0.0', 'all');
+	wp_enqueue_style('bxslidercss', get_template_directory_uri().'/css/jquery.bxslider.css', array(), '1.0.0', 'all');
+	wp_enqueue_style('fancyboxcss', get_template_directory_uri().'/css/jquery.fancybox.css', array(), '1.0.0', 'all');
 	wp_enqueue_style('customstyle', get_template_directory_uri().'/css/kibango.css', array(), '1.0.0', 'all');
 
 	wp_enqueue_script('bootstrapjs', get_template_directory_uri().'/js/bootstrap.min.js', array(), '1.0.0', true);
+	wp_enqueue_script('bxsliderjs', get_template_directory_uri().'/js/jquery.bxslider.min.js', array(), '1.0.0', true);
+	wp_enqueue_script('fancyboxjs', get_template_directory_uri().'/js/jquery.fancybox.js', array(), '1.0.0', true);
 	wp_enqueue_script('customjs', get_template_directory_uri().'/js/main.js', array(), '1.0.0', true);
 }
 
