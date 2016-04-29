0100177e43ff9646ac688e4d17d4f107baddce60
markon87 <marfy87@gmail.com>
2016-04-29 17:38:32 +0200

    bx slider dynamical image load

--- bxslider-page.php
+++ bxslider-page.php
@@ -12,19 +12,6 @@
 </ul> -->
 
 
-<ul class="bxslider">
-  <li><img src="<?php echo content_url().'/uploads/2016/04/cover_01.jpg'?>" /></li>
-  <li><img src="<?php echo content_url().'/uploads/2016/04/cover_02.jpg'?>" /></li>
-  <li><img src="<?php echo content_url().'/uploads/2016/04/cover_03.jpg'?>" /></li>
-  <li><img src="<?php echo content_url().'/uploads/2016/04/cover_04.jpg'?>" /></li>
-  <li><img src="<?php echo content_url().'/uploads/2016/04/cover_05.jpg'?>" /></li>
-  <li><img src="<?php echo content_url().'/uploads/2016/04/cover_06.jpg'?>" /></li>
-  <li><img src="<?php echo content_url().'/uploads/2016/04/cover_07.jpg'?>" /></li>
-  <li><img src="<?php echo content_url().'/uploads/2016/04/cover_08.jpg'?>" /></li>
-  <li><img src="<?php echo content_url().'/uploads/2016/04/cover_09.jpg'?>" /></li>
-  <li><img src="<?php echo content_url().'/uploads/2016/04/cover_10.jpg'?>" /></li>
-</ul>
-
 <?php
 function GetName($parent)
 {
@@ -45,7 +32,7 @@ function GetName($parent)
         {
           if (strpos($el, 'cover_') !== false) 
           {
-              echo '<li>' . $absolutePath  .'</li>';  
+              echo '<li>' . '<img src="' . $absolutePath . '" />' .'</li>';  
           }
         }
       }
@@ -53,9 +40,11 @@ function GetName($parent)
   }
 }
 
-$path    = './images';
-echo '<ul>';
+$path    = content_url() . '/uploads';
+echo '<ul class="bxslider">';
 GetName($path);
 echo '</ul>';
+
+echo $path;
 
 ?>
