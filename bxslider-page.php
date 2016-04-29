fafd70a821fedfec50b8fdca11c9048ab3753830
Milos <milosnovakovic91@gmail.com>
2016-04-29 16:16:10 +0200

    find absolute path by file name

--- bxslider-page.php
+++ bxslider-page.php
@@ -24,3 +24,38 @@
   <li><img src="<?php echo content_url().'/uploads/2016/04/cover_09.jpg'?>" /></li>
   <li><img src="<?php echo content_url().'/uploads/2016/04/cover_10.jpg'?>" /></li>
 </ul>
+
+<?php
+function GetName($parent)
+{
+  if(is_dir($parent))
+  {
+    $list = scandir($parent);
+    foreach($list as $el)
+    {
+
+      if($el != '.' && $el != '..')
+      {
+        $absolutePath = $parent . '/' .$el;
+        if(is_dir($absolutePath))
+        {
+          GetName($absolutePath);
+        }
+        else
+        {
+          if (strpos($el, 'cover_') !== false) 
+          {
+              echo '<li>' . $absolutePath  .'</li>';  
+          }
+        }
+      }
+    }
+  }
+}
+
+$path    = './images';
+echo '<ul>';
+GetName($path);
+echo '</ul>';
+
+?>
